93ed22c3c7528a128f02528854c8750db987dd7f
Oliver Stotzem <5693011+stotzem@users.noreply.github.com>
2025-08-05 12:32:28 +0200

    Rename project to os.ax

---
+++ src/basetemplate.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html><?php global $site ?>
+<html lang="de">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,minimum-scale=1">
+<title><?= $site['title'] ?></title>
+<div class="container">
+    <main><?= $site['main'] ?></main>
+</div>
--- src/core.php
+++ src/core.php
@@ -1,5 +1,12 @@
 <?php
 
+$site = array(
+    'title' => 'stotzem.dev',
+    'main' => '',
+);
+
+$route = 'imprint';
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $env = parse_ini_file(__DIR__ . '/../.env');
@@ -13,6 +20,20 @@ try {
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
         ]
     );
+
+    $stmt = $db->prepare("SELECT content FROM pages WHERE route = ? LIMIT 1");
+    $stmt->execute([$route]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($result !== false) {
+        $site['main'] = $result['content'];
+    }
+    else {
+        $site['main'] = 'not-found';
+    }
 } catch (PDOException $e) {
     die($e->getMessage());
 }
+
+ob_start();
+require 'basetemplate.php';
+ob_end_flush();
