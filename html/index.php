c99023bdda32dc56eb5939707d4b787e8db75604
Oliver Stotzem <5693011+stotzem@users.noreply.github.com>
2025-08-06 12:32:02 +0200

    Add redirection logic

--- html/index.php
+++ html/index.php
@@ -1,3 +1,28 @@
 <?php
 
-phpinfo();
+$env = parse_ini_file("../.env");
+$request = ltrim($_SERVER['REQUEST_URI'], '/');
+$shortcode = explode('/', $request)[0];
+$url = 'https://stotzem.dev';
+
+if (!empty($shortcode)) {
+    try {
+        $db = new PDO(
+            'mysql:host=' . $env['SQL_HOSTNAME'] . ';dbname=' . $env['SQL_DATABASE'],
+            $env['SQL_USERNAME'], $env['SQL_PASSWORD'],
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+
+        $stmt = $db->prepare('SELECT url FROM shortcodes WHERE shortcode = ? LIMIT 1');
+        $stmt->execute([$shortcode]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            $url = $result['url'];
+        }
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+header('HTTP/1.1 302 Found');
+header("Location: $url");
--- src/basetemplate.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html><?php global $site ?>
-<html lang="de">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,minimum-scale=1">
-<title><?= $site['title'] ?></title>
-<div class="container">
-    <main><?= $site['main'] ?></main>
-</div>
